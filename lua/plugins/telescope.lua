6acee661686054c40a839ec4cd2031f772354c01
Sam Cochran <sam@sygaldry.com>
2026-04-01 13:02:36 -0400

    stop excluding files in gitignore from fuzzy find

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,7 +10,9 @@ return {
             }
         })
         local builtin = require("telescope.builtin")
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>ff', function()
+            builtin.find_files({ no_ignore = true, hidden = true })
+        end, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end
     },
